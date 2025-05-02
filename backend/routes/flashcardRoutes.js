5bc8fa4c84096c72d0f870c398e6dff34aff6b8c
Asif Ali Baig <148427844+baigasif42@users.noreply.github.com>
2025-05-02 19:12:42 +0530

    Update flashcardRoutes.js

--- backend/routes/flashcardRoutes.js
+++ backend/routes/flashcardRoutes.js
@@ -1,1 +1,13 @@
+// backend/routes/flashcardRoutes.js
+const express = require('express');
+const flashcardController = require('../controllers/flashcardController');
 
+const router = express.Router();
+
+router.get('/', flashcardController.getFlashcards);
+router.get('/:id', flashcardController.getFlashcard);
+router.post('/', flashcardController.createFlashcard);
+router.post('/generate', flashcardController.createAIFlashcards);
+router.delete('/:id', flashcardController.deleteFlashcard);
+
+module.exports = router;
